2d3e03cee8caa67abbdc8d4ac48262d366808744
Marek Kochanowicz <sirherrbatka@gmail.com>
2021-04-17 17:59:04 +0200

    Added test case.

--- source/interpreter/protocol/object-tests.lisp
+++ source/interpreter/protocol/object-tests.lisp
@@ -1,6 +1,6 @@
 (cl:in-package #:hermetica.interpreter.protocol)
 
-(prove:plan 16)
+(prove:plan 18)
 
 (defclass tested-class2 ()
   ((%test-slot1 :initarg :test-slot1
@@ -145,6 +145,31 @@
   (prove:is (length list) 2)
   (prove:is (first list) 0)
   (prove:is (second list) 1))
+
+
+(bind ((pattern1 (make 'repr:object-node
+                       :object-class (make 'repr:constant-node :value (find-class 'tested-class1))
+                       :children (list (make-instance 'repr:slot-node
+                                                      :slot-reader 'test-slot2
+                                                      :value (make 'repr:free-value-node
+                                                                   :variable-name '?value)))))
+       (pattern2 (make 'repr:object-node
+                       :object-class (make 'repr:constant-node :value (find-class 'tested-class2))
+                       :children (list (make-instance 'repr:slot-node
+                                                      :slot-reader 'test-slot2
+                                                      :value (make 'repr:free-value-node :variable-name '?value)))))
+       (compiled1 (compile-node pattern1))
+       (compiled2 (compile-node pattern2))
+       (context (context *testing-sequence-interface*
+                         (vector (make 'tested-class1
+                                       :test-slot1 1
+                                       :test-slot2 2)
+                                 (make 'tested-class2
+                                       :test-slot1 2
+                                       :test-slot2 1))))
+       ((:values found list) (funcall compiled1 context compiled2)))
+  (prove:ok (not found))
+  (prove:is (length list) 0))
 
 
 (prove:finalize)
